57ad042675d7aa65740f9f744d077a5a896a0de4
Kalk88 <simondmansson@gmail.com>
2019-06-19 22:03:27 +0200

    Health route fix

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 require('dotenv').config()
 const express = require('express')
+const app = express()
 const graphQLHTTP = require('express-graphql')
 const schema = require('./productSchema.js')
-const healthRouter = require('./health.js').router
-const app = express()
-const port = process.env.PORT
+const healthRouter = require('./health.js')
+const port = process.env.PORT || 5005
 
-app.use('/', graphQLHTTP({
+app.use('/graph', graphQLHTTP({
   schema,
   graphiql: true
 }))
 
 app.use(healthRouter)
 
-app.listen(port || 5005, () => console.log(`on port ${port}`))
+app.listen(port, () => console.log(`on port ${port}`))
