3f8d1a4c699fd4b897d14af6c74dcef08c4bd59c
Kalk <simon.d.mansson@gmail.com>
2019-06-23 21:34:25 +0200

    Add rate limit

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 require('dotenv').config()
+const rateLimit = require('express-rate-limit')
 const express = require('express')
 const app = express()
 const graphQLHTTP = require('express-graphql')
 const schema = require('./productSchema.js')
 const healthRouter = require('./health.js')
 const port = process.env.PORT || 5005
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000,
+  max: 100,
+  message: 'To many requests sent from this ip'
+})
+
+app.use(limiter)
 
 app.use('/graph', graphQLHTTP({
   schema,
